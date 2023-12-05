f7a62ac9334f69f6d437909b1d6ffc4794c549ed
mohamedmurra <99559770+mohamedmurra@users.noreply.github.com>
2023-12-05 07:41:31 +0200

    Update index.js

--- src/pages/PropertDetail/index.js
+++ src/pages/PropertDetail/index.js
@@ -194,7 +194,7 @@ const PropertyDetail = () => {
               </div>
               <div className='price-detail'>
                 <Typography
-                  sx={{ fontSize: { md: '2rem', xs: '1.5rem' } }}
+                  sx={{ display:'flex', flexDirection:'row',flexWrap:'noWrap,fontSize: { md: '2rem', xs: '1rem' } }}
                   textAlign='left'
                   variant='h4'
                   component='h2'
